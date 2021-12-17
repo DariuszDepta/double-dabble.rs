9db1dfdf57152906120e3b97f1e0b2d6a8425d6a
Dariusz Depta <dariusz.depta@engos.software>
2021-12-17 20:30:39 +0100

    Updated lib interface.

--- benches/bin2bcd.rs
+++ benches/bin2bcd.rs
@@ -4,12 +4,12 @@ extern crate test;
 
 #[cfg(test)]
 mod tests {
-  use bcd::bin2bcd128;
+  use bcd::{bin2bcd128, BCD_DIGITS_128};
   use test::Bencher;
 
   #[bench]
   fn bench_bcd128_1_digit(b: &mut Bencher) {
-    let mut bcd = [0; 34];
+    let mut bcd = [0; BCD_DIGITS_128];
     b.iter(|| {
       bin2bcd128(1, &mut bcd);
     });
@@ -17,7 +17,7 @@ mod tests {
 
   #[bench]
   fn bench_bcd128_8_digits(b: &mut Bencher) {
-    let mut bcd = [0; 34];
+    let mut bcd = [0; BCD_DIGITS_128];
     b.iter(|| {
       bin2bcd128(89999999, &mut bcd);
     });
@@ -25,7 +25,7 @@ mod tests {
 
   #[bench]
   fn bench_bcd128_16_digits(b: &mut Bencher) {
-    let mut bcd = [0; 34];
+    let mut bcd = [0; BCD_DIGITS_128];
     b.iter(|| {
       bin2bcd128(8999999999999999, &mut bcd);
     });
@@ -33,7 +33,7 @@ mod tests {
 
   #[bench]
   fn bench_bcd128_32_digits(b: &mut Bencher) {
-    let mut bcd = [0; 34];
+    let mut bcd = [0; BCD_DIGITS_128];
     b.iter(|| {
       bin2bcd128(89999999999999999999999999999999, &mut bcd);
     });
@@ -41,7 +41,7 @@ mod tests {
 
   #[bench]
   fn bench_bcd128_34_digits(b: &mut Bencher) {
-    let mut bcd = [0; 34];
+    let mut bcd = [0; BCD_DIGITS_128];
     b.iter(|| {
       bin2bcd128(8999999999999999999999999999999999, &mut bcd);
     });
--- src/bcd.rs
+++ src/bcd.rs
@@ -1,40 +1,7 @@
 ///
-const BCD_BITS_8: usize = 8;
-///
-const BCD_DIGITS_8: usize = 3;
-
-///
-pub fn bin2bcd8(bin: u8, bcd: &mut [u8; BCD_DIGITS_8]) {
-  let mask: u8 = 1 << (BCD_BITS_8 - 1);
-  let mut carry = [0; BCD_DIGITS_8];
-  let mut num = bin;
-  for _ in 0..BCD_BITS_8 {
-    let mut k = BCD_DIGITS_8 - 1;
-    if bcd[k] > 4 {
-      bcd[k] += 3;
-    }
-    carry[k] = (bcd[k] & 0x8) >> 3;
-    if (num & mask) > 0 {
-      bcd[k] = ((bcd[k] << 1) & 0xF) | 0x1;
-    } else {
-      bcd[k] = (bcd[k] << 1) & 0xF;
-    }
-    while k > 0 {
-      k -= 1;
-      if bcd[k] > 4 {
-        bcd[k] += 3;
-      }
-      carry[k] = (bcd[k] & 0x8) >> 3;
-      bcd[k] = ((bcd[k] << 1) | carry[k + 1]) & 0xF;
-    }
-    num <<= 1;
-  }
-}
-
+pub const BCD_DIGITS_128: usize = 34;
 ///
 const BIN_BITS_128: usize = 128;
-///
-const BCD_DIGITS_128: usize = 34;
 
 ///
 pub fn bin2bcd128(bin: u128, bcd: &mut [u8; BCD_DIGITS_128]) {
